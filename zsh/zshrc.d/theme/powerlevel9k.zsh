a3c7862e69531d1102958e239b16091794835b86
kawaii-kuakka <kawaii.kuakka@gmail.com>
2015-11-02 08:08:36 +0900

    Add powerlevel9k settings

--- zsh/zshrc.d/theme/powerlevel9k.zsh
+++ zsh/zshrc.d/theme/powerlevel9k.zsh
@@ -1,5 +1,27 @@
 # Use 256 color terminal
 export TERM="xterm-256color"
 
-# Use more glyphs
-POWERLEVEL9K_MODE='awesome-patched'
+# Use more glyphs and flat separator
+POWERLEVEL9K_MODE='flat'
+
+# Use doubole-lined prompt
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="↱"
+POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX="↳ "
+
+# Select segments
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status)
+
+# Context segment
+export DEFAULT_USER='nbchk'
+
+# Dir segment
+POWERLEVEL9K_DIR_BACKGROUND='cyan'
+
+# VCS segment
+POWERLEVEL9K_SHOW_CHANGESET=true
+POWERLEVEL9K_CHANGESET_HASH_LENGTH=6
+
+# Status segment
+POWERLEVEL9K_STATUS_VERBOSE=false
