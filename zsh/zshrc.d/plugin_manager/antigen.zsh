064284f1ad19982bc45f71e1aff1a7d8d7fb1c0c
kawaii-kuakka <kawaii.kuakka@gmail.com>
2015-11-02 08:47:37 +0900

    Install and enable zsh-completions on zsh

--- zsh/zshrc.d/plugin_manager/antigen.zsh
+++ zsh/zshrc.d/plugin_manager/antigen.zsh
@@ -4,5 +4,8 @@ source ~/.zsh/antigen/antigen.zsh
 # Theme
 antigen theme bhilburn/powerlevel9k powerlevel9k
 
+# Completion
+antigen bundle zsh-users/zsh-completions src
+
 # Tell antigen that you're done.
 antigen apply
